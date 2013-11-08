ce25091715894e57fbde2aa88c56f932c346000e
Edwin Kempin <edwin.kempin@sap.com>
2013-11-08 13:54:20 +0100

    Initial commit
    This plugin allows to create service users in Gerrit.
    
    A service user is a user that is used by another service to
    communicate with Gerrit. E.g. a service user is needed to run the
    Gerrit Trigger Plugin in Jenkins. A service user is not able to login
    into the Gerrit WebUI and it cannot push commits and tags.
    
    This plugin supports to create service users via SSH, REST and in the
    WebUI.
    
    To be able to create a service user a user must be a member of a group
    that is granted the 'Create Service User' capability (provided by this
    plugin) or the 'Administrate Server' capability.
    
    On creation new service users can be automatically added to groups
    that are configured in the plugin. This allows to automatically assign
    or block certain access rights for the service users.
    
    At the moment the creator cannot directly add a new service user on
    creation to groups as it is possible with the '--group' parameters of
    the 'create-account' SSH command. This could be added later.
    
    Change-Id: Ib0106574f7d8f22b50c81364e3437ad3716ba879
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/serviceuser/ServiceUserInput.java
@@ -0,0 +1,30 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.serviceuser;
+
+import com.google.gerrit.server.account.CreateAccount;
+import com.google.gerrit.server.config.PluginConfig;
+
+import java.util.Arrays;
+
+public class ServiceUserInput extends CreateAccount.Input {
+
+  public ServiceUserInput(String username, String sshKey, PluginConfig cfg) {
+    this.username = username;
+    this.name = username;
+    this.sshKey = sshKey;
+    this.groups = Arrays.asList(cfg.getStringList("group"));
+  }
+}
---
+++ src/main/resources/Documentation/build.md
@@ -0,0 +1,12 @@
+Build
+=====
+
+This plugin is built with Maven.
+
+```
+  mvn clean package
+```
+
+The plugin build requires the Gerrit Plugin API to be available.
+How to build the Gerrit Plugin API is described in the [Gerrit
+documentation](../../../Documentation/dev-buck.html#_extension_and_plugin_api_jar_files).
