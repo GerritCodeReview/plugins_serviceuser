0e676a5702cebb6112ac460bfa50abdc10e2b68d
Edwin Kempin <edwin.kempin@sap.com>
2014-02-08 16:13:39 +0100

    Fix NPE if unauthenticated user tries to list service users
    Handle unauthenticated requests and fail with 403 Forbidden saying
    that authentication is required.
    
    Change-Id: Id75a650ae57a9279b92e75552b558efc2b74140e
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- src/main/java/com/googlesource/gerrit/plugins/serviceuser/ServiceUserMenu.java
+++ src/main/java/com/googlesource/gerrit/plugins/serviceuser/ServiceUserMenu.java
@@ -16,6 +16,7 @@ package com.googlesource.gerrit.plugins.serviceuser;
 
 import com.google.common.collect.Lists;
 import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.webui.TopMenu;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.account.CapabilityControl;
@@ -72,7 +73,7 @@ public class ServiceUserMenu implements TopMenu {
   private boolean hasServiceUser() {
     try {
       return !listServiceUsers.get().apply(new ConfigResource()).isEmpty();
-    } catch (OrmException e) {
+    } catch (AuthException | OrmException e) {
       return false;
     }
   }
