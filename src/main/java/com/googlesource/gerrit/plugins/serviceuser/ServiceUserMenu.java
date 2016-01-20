26e95c108c8c0ba53afc5d7e3a526bf2028253da
David Pursehouse <david.pursehouse@sonymobile.com>
2016-01-20 12:33:00 +0900

    Adapt to upstream move of MenuItem
    In I5b879c4f MenuItem was extracted to its own class.
    
    Change-Id: Ib5ca2dd332ccaaca16b1e30e31c9675dddf75513

--- src/main/java/com/googlesource/gerrit/plugins/serviceuser/ServiceUserMenu.java
+++ src/main/java/com/googlesource/gerrit/plugins/serviceuser/ServiceUserMenu.java
@@ -16,6 +16,7 @@ package com.googlesource.gerrit.plugins.serviceuser;
 
 import com.google.common.collect.Lists;
 import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.extensions.client.MenuItem;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.webui.TopMenu;
 import com.google.gerrit.server.CurrentUser;
