80a9967f3a9980a0749d247c5acf597925fb7dab
Thomas Draebing <thomas.draebing@sap.com>
2019-08-02 16:52:37 +0200

    Add PolyGerrit UI to list service users
    The GWT-based UI was removed recently to allow compatibility with Gerrit
    3.0+.
    
    This change adds a screen listing all service users as a first step to
    reimplement the UI with PolyGerrit. The new screen can be found under
    'Browse' > 'Service Users'.
    
    The list does currently not support pagination, since the REST API of
    the plugin does not support it yet. Also, filtering is currently not
    implemented, since the router does not support query operators for
    plugin screens.
    
    Change-Id: Ibcbfac311e9a279d14dc92299e6d963cef19a87b

---
+++ src/main/java/com/googlesource/gerrit/plugins/serviceuser/ServiceUserMenu.java
@@ -0,0 +1,71 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.serviceuser;
+
+import static com.google.gerrit.server.permissions.GlobalPermission.ADMINISTRATE_SERVER;
+
+import com.google.common.collect.Lists;
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.extensions.api.access.PluginPermission;
+import com.google.gerrit.extensions.client.MenuItem;
+import com.google.gerrit.extensions.webui.TopMenu;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import java.util.List;
+
+class ServiceUserMenu implements TopMenu {
+  private final String pluginName;
+  private final Provider<CurrentUser> userProvider;
+  private final List<MenuEntry> menuEntries;
+  private final PermissionBackend permissionBackend;
+
+  @Inject
+  ServiceUserMenu(
+      @PluginName String pluginName,
+      Provider<CurrentUser> userProvider,
+      PermissionBackend permissionBackend) {
+    this.pluginName = pluginName;
+    this.userProvider = userProvider;
+    menuEntries = Lists.newArrayList();
+    this.permissionBackend = permissionBackend;
+
+    List<MenuItem> menuItems = Lists.newArrayListWithExpectedSize(2);
+    if (canCreateServiceUser()) {
+      menuItems.add(new MenuItem("Service Users", "#/x/" + pluginName + "/list"));
+    }
+    if (!menuItems.isEmpty()) {
+      menuEntries.add(new MenuEntry("Browse", menuItems));
+    }
+  }
+
+  private boolean canCreateServiceUser() {
+    if (userProvider.get().isIdentifiedUser()) {
+      IdentifiedUser user = userProvider.get().asIdentifiedUser();
+      return permissionBackend
+              .user(user)
+              .testOrFalse(new PluginPermission(pluginName, CreateServiceUserCapability.ID))
+          || permissionBackend.user(user).testOrFalse(ADMINISTRATE_SERVER);
+    }
+    return false;
+  }
+
+  @Override
+  public List<MenuEntry> getEntries() {
+    return menuEntries;
+  }
+}
---
+++ src/main/resources/static/gr-serviceuser-list.js
@@ -0,0 +1,75 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+(function () {
+  'use strict';
+
+  Polymer({
+    is: 'gr-serviceuser-list',
+    _legacyUndefinedCheck: true,
+
+    properties: {
+      _serviceUsers: Array,
+      _loading: {
+        type: Boolean,
+        value: true,
+      },
+    },
+
+    behaviors: [Gerrit.ListViewBehavior,],
+
+    attached() {
+      this.fire('title-change', { title: 'Service Users' });
+      this._getServiceUsers();
+    },
+
+    _getServiceUsers() {
+      return this.plugin.restApi('/config/server/serviceuser~serviceusers/').get('')
+        .then(serviceUsers => {
+          if (!serviceUsers) {
+            this._serviceUsers = [];
+            return;
+          }
+          this._serviceUsers = Object.keys(serviceUsers)
+            .map(key => {
+              const serviceUser = serviceUsers[key];
+              serviceUser.username = key;
+              return serviceUser;
+            });
+          this._loading = false;
+        });
+    },
+
+    _active(item) {
+      return item.inactive === true ? 'Inactive' : 'Active';
+    },
+
+    _getCreator(item) {
+      if (item.created_by.username != undefined) {
+        return item.created_by.username;
+      }
+      if (item.created_by._account_id != -1) {
+        return item.created_by._account_id;
+      }
+
+      return "N/A";
+    },
+
+    _getOwnerGroup(item) {
+      if (item.owner) {
+        return item.owner.name;
+      }
+    },
+  })
+})();
