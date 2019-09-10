710ef718d044dfa98f1dd99cae960c9c9eebe0cc
Thomas Draebing <thomas.draebing@sap.com>
2019-09-10 16:28:22 +0200

    Allow additional SSH-key formats
    So far only RSA-type SSH-keys were allowed to be used for service users.
    Since Gerrit and older versions of this plugin allowed other formats
    as well, this could break existing installations.
    
    This change adds a validator-class for SSH-keys that is able to validate
    a wider range of SSH-key formats. The validation is far from perfect and
    only meant to minimize the risk of creating unusable accounts, because
    of an invalid SSH-key.
    
    Change-Id: I3396ee449f6f88708514583a055a45c69abb4dd9

---
+++ src/main/java/com/googlesource/gerrit/plugins/serviceuser/ValidateSshKey.java
@@ -0,0 +1,55 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.serviceuser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class ValidateSshKey {
+
+  private static final String OPENSSH_KEY_PREFIXES[] = {
+    "ssh-ed25519", "ssh-rsa", "ssh-dss", "ecdsa-sha2-"
+  };
+  private static final String RFC_KEY_FORMAT_PATTERN =
+      "(?s)^-{4,5}\\s?BEGIN.* PUBLIC KEY\\s?-{4,5}.+-{4,5}\\s?END.* PUBLIC KEY\\s?-{4,5}$";
+
+  private static boolean validateOpenSshPrefix(String sshKey) {
+    for (String prefix : OPENSSH_KEY_PREFIXES) {
+      if (sshKey.startsWith(prefix)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private static boolean validateRfcSshKey(String sshKey) {
+    Pattern pattern = Pattern.compile(RFC_KEY_FORMAT_PATTERN);
+    Matcher matcher = pattern.matcher(sshKey);
+    return matcher.find();
+  }
+
+  static boolean validateSshKeyFormat(String sshKey) {
+    if (sshKey == null) {
+      return false;
+    }
+
+    if (validateRfcSshKey(sshKey)) {
+      return true;
+    }
+
+    return validateOpenSshPrefix(sshKey);
+  }
+}
