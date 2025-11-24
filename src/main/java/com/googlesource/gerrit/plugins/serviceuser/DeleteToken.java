376668f803489b970400fae72ca36f0089430399
Thomas Dräbing <thomas.draebing@sap.com>
2025-11-24 15:58:58 +0100

    Fix capability of serviceuser owners to delete auth tokens
    Change-Id: I8d17ec694fca8dcd24b0a3087f85a5b3aae01ce9

--- src/main/java/com/googlesource/gerrit/plugins/serviceuser/DeleteToken.java
+++ src/main/java/com/googlesource/gerrit/plugins/serviceuser/DeleteToken.java
@@ -21,6 +21,7 @@ import com.google.gerrit.extensions.restapi.Response;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.extensions.restapi.RestModifyView;
 import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.account.AuthTokenAccessor;
 import com.google.gerrit.server.account.InvalidAuthTokenException;
 import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.config.PluginConfigFactory;
@@ -30,14 +31,13 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.serviceuser.email.ServiceUserOutgoingEmail;
-import com.googlesource.gerrit.plugins.serviceuser.email.ServiceUserUpdatedEmailDecorator.Operation;
 import java.io.IOException;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 
 @Singleton
 public class DeleteToken implements RestModifyView<ServiceUserResource.Token, Input> {
   private final PluginConfig config;
-  private final com.google.gerrit.server.restapi.account.DeleteToken deleteToken;
+  private final AuthTokenAccessor tokenAccessor;
   private final Provider<CurrentUser> self;
   private final PermissionBackend permissionBackend;
   private final ServiceUserOutgoingEmail.Factory outgoingEmailFactory;
@@ -46,12 +46,12 @@ public class DeleteToken implements RestModifyView<ServiceUserResource.Token, In
   DeleteToken(
       @PluginName String pluginName,
       PluginConfigFactory pluginConfigFactory,
-      com.google.gerrit.server.restapi.account.DeleteToken deleteToken,
+      AuthTokenAccessor tokenAccessor,
       Provider<CurrentUser> self,
       PermissionBackend permissionBackend,
       ServiceUserOutgoingEmail.Factory outgoingEmailFactory) {
     this.config = pluginConfigFactory.getFromGerritConfig(pluginName);
-    this.deleteToken = deleteToken;
+    this.tokenAccessor = tokenAccessor;
     this.self = self;
     this.permissionBackend = permissionBackend;
     this.outgoingEmailFactory = outgoingEmailFactory;
@@ -69,10 +69,7 @@ public class DeleteToken implements RestModifyView<ServiceUserResource.Token, In
       permissionBackend.user(self.get()).check(ADMINISTRATE_SERVER);
     }
 
-    Response<String> resp = deleteToken.apply(rsrc.getUser(), rsrc.getToken().id(), false);
-    if (resp.statusCode() == Response.none().statusCode()) {
-      outgoingEmailFactory.create(rsrc, Operation.DELETE_TOKEN).send();
-    }
-    return resp;
+    tokenAccessor.deleteToken(rsrc.getUser().getAccountId(), rsrc.getToken().id());
+    return Response.none();
   }
 }
